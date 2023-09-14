aff23f5cf99c4dd37fafa1c504add1ade330ea4b
ckush <ckush@vmware.com>
2023-09-14 19:30:31 +0100

    Swagger UI

---
+++ plantastic-app/src/main/java/com/plantastic/OpenApiConfig.java
@@ -0,0 +1,14 @@
+/*
+ * ******************************************************
+ * Copyright VMware, Inc. 2023.   All Rights Reserved.
+ * ******************************************************
+ */
+package com.plantastic;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+
+}
